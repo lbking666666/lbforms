e9a950f674ed4277edadb1a18aceff373c1581ad
lvbin <lbking666666@qq.com>
2016-12-06 09:33:12 +0800

    lbforms

--- js/lb.form.js
+++ js/lb.form.js
@@ -13,6 +13,45 @@
 		this._init(ele);//初始化
 	};
 
+	function lbSelect(a){
+		var g = [], b = '', h = '';
+		h = $(a).attr('lb-select').split(';');
+		g.push('<ul class="lb-input-select-list">');
+		for(var i = 0; i < h.length; i++){
+			b = eval("("+h[i]+")");
+			g.push('<li id="'+b.id+'">'+b.value+'</li>');
+		}
+		g.push('</ul>');
+		$(a).parents('.lb-form').append(g.join(''));
+	}
+
+	function lbFile(a,b,ft){
+		$(a).parents('.lb-form').append(b);
+		$(a).parents('.lb-form').append('<span class="lb-file-tip">'+ft+'</span>')
+		$(a).attr('type','hide');
+		$(a).removeAttr('id');
+	}
+	function _w(c,b){
+		var d = 0 ,w = 0;
+		if(c.length > 1){
+			for(var i = 0; i < c.length; i++){
+				d += parseInt(c.eq(i).outerWidth(true));
+			}	
+		}else{
+			d = parseInt(c.outerWidth(true));
+		}
+		return w = b - d - 2;
+	}
+
+	function getNowFormatDate() {//获得日期格式为20161202的日期
+		var mh, sd, cd;
+	    mh = new Date().getMonth() + 1;
+	    sd = new Date().getDate();
+	    if (mh >= 1 && mh <= 9) (mh = "0" + mh);
+	    if (sd >= 0 && sd <= 9) (sd = "0" + sd);
+	    return cd = new Date().getFullYear() + '' + mh + '' + sd;
+	}
+
 	lbform.prototype = {
 		/**
 			@a:当前的容器
@@ -34,9 +73,10 @@
 			function g(){
 				setTimeout(function(){
 					for(var i = 0;i<a.length;i++){
-						var b = $(a[i]).parents('.lb-form').parent().width();
-						if($(window).outerWidth(true)%2 != 0) var b = b-1
-						c._g(a[i],b);
+						var b, d;
+						b = $(a[i]).parents('.lb-form').parent().width();
+						($(window).outerWidth(true)%2 != 0)?(d=b-1):(d=b);
+						c._g(a[i],d);
 					}
 				},50);
 			}
@@ -54,41 +94,34 @@
 					创建父级lb-form添加到当前input的外层;
 					根据type判断input的类型：
 					1.INPUT:
-						如果为下拉输入框，则把data-select的值转化为ul>li形式插入到input之后,添加select事件函数;
+						如果为下拉输入框，则把lb-select的值转化为ul>li形式插入到input之后,添加select事件函数;
 						如果为上传文件，则复制当前input，去掉当前input的id，类型改变为hide，把复制后的b使用uploadfiy插件；
 					2.SELECT:
 						添加change事件	
 			***/
-			var c = this;
-			var type = a.tagName;
-			var f = $('<div class="lb-form" style="float:right"></div>');
+			var c, type, f;
+			c = this;
+			type = a.tagName;
+			f = $('<div class="lb-form" style="float:right"></div>');
 			$(a).wrap(f);
-			if(type == "INPUT"){
-				if($(a).attr('data-select')){
-					var g = [];
-					var h = $(a).attr('data-select').split(';');
-					g.push('<ul class="lb-input-select-list">');
-					for(var i = 0; i<h.length;i++){
-						var obj = eval("("+h[i]+")")
-						g.push('<li id="'+obj.id+'">'+obj.value+'</li>')
+			switch(type){
+				case "INPUT":
+					if($(a).attr('lb-select')){
+						lbSelect(a);
+						c._select(a);
 					}
-					g.push('</ul>');
-					$(a).parents('.lb-form').append(g.join(''));
-					c._select(a);
-				}
-				if($(a).attr('data-file')){
-					var b = $(a).clone();
-					$(a).parents('.lb-form').append(b);
-					var ft = $(a).attr('placeholder');
-					$(a).parents('.lb-form').append('<span class="lb-file-tip">'+ft+'</span>')
-					$(a).attr('type','hide');
-					$(a).removeAttr('id');
-					c._uploadfile(a,b);
-				}
-				c._blurEvent(a,type);
-			}
-			if(type == "SELECT"){
-				c._changeEvent(a,type);
+					if($(a).attr('lb-file')){
+						var b,ft;
+						b = $(a).clone();
+						ft = $(a).attr('placeholder');
+						lbFile(a,b,ft)
+						c._uploadfile(a,b);
+					}
+					c._blurEvent(a,type);
+					break;
+				case "SELECT":
+					c._changeEvent(a,type);
+					break;
 			}
 		},
 		_g : function(a,b){
@@ -100,38 +133,26 @@
 					b-d等于当前的lb-form的宽度 再减去边框2为当前的lb-form的宽度
 			***/
 			var c = $(a).parents('.lb-form').siblings().not('.lb-file');
-			var d = 0;
-			if(c.length > 1){
-				for(var i = 0; i < c.length; i++){
-					d += parseInt(c.eq(i).outerWidth(true));
-				}	
-			}else{
-				d = parseInt(c.outerWidth(true));
-			}
-			var w = b - d - 2;
-			$(a).parents('.lb-form').css({"width":w});
+			$(a).parents('.lb-form').css({"width":_w(c,b)});
 		},
 		_blurEvent:function(a,b){//input光标离开时事件
 			$(a).bind('blur',function(){
-				if($(a).attr('data-tip') == "true"){//判断是否需要提示
-					t(a,b);
-				}
+				lbMsgTip(a,b);
 			})
 		},
 		_changeEvent:function(a,b){//select下拉选择事件
 			$(a).bind('change',function(){
-				if($(a).attr('data-tip') == "true"){//判断是否需要提示
-					t(a,b);
-				}
+				lbMsgTip(a,b);
 			});
 		},
 		_select:function(a){//模拟下拉
 			$(a).bind('click',function(event){
-				var c = $(this);
-				var b = $(this).next('.lb-input-select-list');
-				var bh = $(window).height();
-				var t = $(this).offset().top + 31;
-				var h = b.find('li').length *23;
+				var c, b, d, bh, t, h;
+				c = $(this);
+				b = $(this).next('.lb-input-select-list');
+				bh = $(window).height();
+				t = $(this).offset().top + 31;
+				h = b.find('li').length *23;
 				((bh - t)< h)?(b.css("top",-h)):(b.css("top","31px"));
 				$(document).bind('click',function(){
 					b.hide();
@@ -141,7 +162,7 @@
 				});
 				b.toggle();
 				b.find('li').bind('click',function() {
-					var d = $(this).html();
+					d = $(this).html();
 					c.val(d);
 					b.hide();
 				});
@@ -157,38 +178,14 @@
 					自定义插件的文件类型和按钮名称；
 					成功后回调事件中创建弹出层显示文件或图片
 			***/
-			function getNowFormatDate() {//获得日期格式为20161202的日期
-			    var date = new Date();
-			    var s = "";
-			    var mh = date.getMonth() + 1;
-			    var sd = date.getDate();
-			    if (mh >= 1 && mh <= 9) {
-			        mh = "0" + mh;
-			    }
-			    if (sd >= 0 && sd <= 9) {
-			        sd = "0" + sd;
-			    }
-			    var cd = date.getFullYear() + s + mh + s + sd;
-			    return cd;
-			}
-
-			var bn = $(a).attr('data-btname')|| "上传图片";
-			var y = $(a).attr('data-upType');
-			var n = $(a).attr('data-upnumber');
-			var u = 'http://xiaoyaoge.me/lbforms/upload/'+getNowFormatDate();//文件路径
-			if(y == "img"){
-				var d = 'Image Files';
-				var x = '*.gif; *.jpg; *.png; *.jpeg';
-			}else{
-				var d = 'All Files';
-				var x = '*';
-			}
-			if(n=="1"){
-				var m = false;
-			}else{
-				var m = true;
-			}
-			$(b).uploadify({
+			var bn, y, n, u, m, x, d;
+			bn = $(a).attr('lb-upload-btn')|| "上传";
+			y = $(a).attr('lb-upload-type');//文件类型
+			n = $(a).attr('lb-upload-number');//文件个数
+			u = 'http://xiaoyaoge.me/lbforms/upload/'+getNowFormatDate();//文件路径
+			(y == "img")?(d = 'Image Files',x = '*.gif; *.jpg; *.png; *.jpeg'):(d = 'All Files',x = '*');
+			(n =="1")?(m = false):(m= true);
+			var set = {
 				'buttonText' : bn,
 				'fileTypeDesc' : d,
 				'fileTypeExts' : x,
@@ -201,9 +198,9 @@
 		            if(fi){
 		            	fi.remove();
 		            }
-		            if($('body').find('.lb-pop')){
+		           /* if($('body').find('.lb-pop')){
 						$('.lb-pop').remove();
-					}
+					}*/
 		        },
 				'onUploadSuccess' : function(file, data, response) {
 					$('#' + file.id).parents('.lb-form').find('.lb-file-tip').hide();
@@ -242,7 +239,8 @@
 						});
 					});
 				 }
-			});
+			}
+			$(b).uploadify(set);
 		}
 	};
 
@@ -257,9 +255,9 @@
 		/***
 			ms(a,s)添加提示信息 
 			@a: 当前input
-			@s: 当前input的data-msg
+			@s: 当前input的lb-msg
 				创建提示信息结构
-				判断当前input是否存在data-msg
+				判断当前input是否存在lb-msg
 				判断是否已经存在提示
 				给当前input添加提示并给父级添加error样式
 		***/
@@ -285,8 +283,8 @@
 			@s: 当前input的type类型INPUT,SELECT
 				1.INPUT:
 					input先判断是否为空;
-					为空显示data-msg提示信息;
-					不为空查看input的data-type用正则判断;
+					为空显示lb-msg提示信息;
+					不为空查看input的lb-type用正则判断;
 					如果不符合显示验证信息;
 					再次输入时隐藏掉错误提示.
 				2.SELECT:
@@ -295,32 +293,51 @@
 					再次选择时隐藏提示信息.
 
 		***/
-		var s = $(a).attr('data-msg');
+		var s, k;
+		s = $(a).attr('lb-msg');
+		k= $(a).attr('lb-msg-num');
+		if(k){
+			var sk, sk1, sk2;
+			sk = k.split(",");
+			sk1 = sk[0];
+			sk2 = sk[1];
+		}
 		if(b == "INPUT"){
-			var v = $(a).val();
-			var t = $(a).attr('data-type');
+			var v, t;
+			v = $(a).val();
+			t = $(a).attr('lb-msg-type');
 			var g = {
 		        tel : /^0?1[3|4|5|8][0-9]\d{8}$/,//手机
 		        mail:/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/,//邮箱
-		        name : /^[a-z0-9_-]{3,16}$///用户名
+		        number : /^[0-9]*$/
 		    }
 			if(v == ""){
 				ms(a,s);
 			}else{
+				var m;
 				switch (t){
-					case 'name':
-						var m = '请输入正确名称！';
-						if(!g.name.test(v)) ms(a,m);
+					case 'number':
+						m = '请输入数字！';
+						if(!g.number.test(v)) ms(a,m);
 						break;
 					case 'tel':
-						var m = '请输入正确手机号！';
+						m = '请输入正确手机号！';
 						if(!g.tel.test(v)) ms(a,m);
 						break;
 					case 'mail':
-						var m = '请输入正确邮箱！';
+						m = '请输入正确邮箱！';
 						if(!g.mail.test(v)) ms(a,m);
 						break;
+					default :
+						if(k){
+							if(v.length>sk2 || v.length<sk1){
+								m = '请输入'+sk1+'到'+sk2+'个字符';
+								ms(a,m)
+							}
+						}
+						break;
 				}
+				
 			}
 			$(a).bind('keydown',function(){
 				$(this).parents('.lb-form').removeClass('error');
@@ -328,15 +345,22 @@
 			});
 		}
 		if(b == "SELECT"){
-			var index = $(a).find('option:selected').index();
+			var index, m;
+			index = $(a).find('option:selected').index();
 			if(index == 0){
-				var m = $(a).attr('data-msg');
+				m = $(a).attr('lb-msg');
 				ms(a,m);
 			}
 			$(a).bind('change',function(){
 				$(this).parents('.lb-form').removeClass('error');
 				$(this).next('.lb-msg').hide();
 			});
+		}
+	}
+
+	function lbMsgTip(a,b){
+		if($(a).attr('lb-msg-tip') == "true"){//判断是否需要提示
+			t(a,b);
 		}
 	}
 
